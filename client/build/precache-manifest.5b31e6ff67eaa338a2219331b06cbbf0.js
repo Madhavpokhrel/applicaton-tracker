e46d1bd48786a06126de76e50ba4e51cbc16822f
Madhavpokhrel <Madhavpokhrel05@gmail.com>
2020-04-13 15:32:02 -0400

    final

---
+++ client/build/precache-manifest.5b31e6ff67eaa338a2219331b06cbbf0.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef8ffd612d4ac304805ccfdb9c985aee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc0a200a20d9e2e86c3c",
+    "url": "/static/css/main.aa2e4b4f.chunk.css"
+  },
+  {
+    "revision": "0e8da7c0561776211626",
+    "url": "/static/js/2.b6829aa4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b6829aa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc0a200a20d9e2e86c3c",
+    "url": "/static/js/main.24fc6ca8.chunk.js"
+  },
+  {
+    "revision": "8109597634622d84c91d",
+    "url": "/static/js/runtime-main.032316f9.js"
+  },
+  {
+    "revision": "2d330bffa435aad0b7fe8cebcdf6fc76",
+    "url": "/static/media/1586341143049-Screenshot_3.2d330bff.png"
+  },
+  {
+    "revision": "2d330bffa435aad0b7fe8cebcdf6fc76",
+    "url": "/static/media/1586341219381-Screenshot_3.2d330bff.png"
+  },
+  {
+    "revision": "5f5dbe560d8250d44046247aacc9e0e7",
+    "url": "/static/media/1586373959957-image.5f5dbe56.png"
+  },
+  {
+    "revision": "dfd95d22f9e5afe1e0ef67d053fe6a5f",
+    "url": "/static/media/1586382355732-5966004881195008.dfd95d22.png"
+  },
+  {
+    "revision": "5f5dbe560d8250d44046247aacc9e0e7",
+    "url": "/static/media/1586382702218-image.5f5dbe56.png"
+  },
+  {
+    "revision": "5f5dbe560d8250d44046247aacc9e0e7",
+    "url": "/static/media/1586488657675-image.5f5dbe56.png"
+  },
+  {
+    "revision": "261baf82d931ae6539f5ca1288f2db3e",
+    "url": "/static/media/1586552946996-natural_image.261baf82.jpg"
+  },
+  {
+    "revision": "65455395f49a142dd3a4acc52384f8bb",
+    "url": "/static/media/1586646997910-resume.65455395.png"
+  },
+  {
+    "revision": "65455395f49a142dd3a4acc52384f8bb",
+    "url": "/static/media/1586647138158-resume.65455395.png"
+  },
+  {
+    "revision": "e287f89f2260f971c43bfcb281f72848",
+    "url": "/static/media/1586749158494-wallpaper.e287f89f.jpeg"
+  },
+  {
+    "revision": "598ead1efa2b3ed83f05705aaa91c853",
+    "url": "/static/media/1586795541108-frontEndDeveloper.598ead1e.png"
+  },
+  {
+    "revision": "7922261dbbcc2426e15e2afe0c5b3743",
+    "url": "/static/media/1586795612753-webDeveloper.7922261d.png"
+  },
+  {
+    "revision": "ed69d6c2ae9a734cccea8e3fe07f5644",
+    "url": "/static/media/1586801833684-fullStackDeveloper.ed69d6c2.png"
+  },
+  {
+    "revision": "ed69d6c2ae9a734cccea8e3fe07f5644",
+    "url": "/static/media/1586801834018-fullStackDeveloper.ed69d6c2.png"
+  },
+  {
+    "revision": "ed69d6c2ae9a734cccea8e3fe07f5644",
+    "url": "/static/media/1586801869867-fullStackDeveloper.ed69d6c2.png"
+  },
+  {
+    "revision": "30ffc262934acec9f5561048b69d5197",
+    "url": "/static/media/img.30ffc262.png"
+  }
+]);
--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express= require('express')
 const PORT  = process.env.PORT||5000
 const applicationRouter= require('./router/applicationRouter')
 const app = express()
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const path = require('path');
 
 app.use(express.json())
 app.use(applicationRouter)
+app.use(express.static('build'));
 
-
+app.get('*',*(req,res)=>{
+	res.sendFile(path.join(__dirname, 'client/build/index.html'));
+})
 mongoose.connect('mongodb://localhost/applicatondata',{useNewUrlParser:true, useFindAndModify:false, useUnifiedTopology:true})
 .then(()=>{
 	console.log("mongodb connected successfull ")
