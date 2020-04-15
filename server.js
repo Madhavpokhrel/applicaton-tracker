c30aee7bdd4a19afede5766d2a17ce5729a70b84
Madhavpokhrel <Madhavpokhrel05@gmail.com>
2020-04-15 15:58:23 -0400

    server file change

--- server.js
+++ server.js
@@ -10,8 +10,12 @@ app.use(applicationRouter)
 app.use(express.static(path.join(__dirname,'client/build')));
 
 app.get('/',(req,res)=>{
-	res.sendFile(path.join(__dirname, 'client/build','index.html'));
+	res.sendFile(path.join(__dirname, 'client/build','index.html
 })
+if(process.env.NODE_ENV==='production'){
+	app.use(express.static('client/build'));
+}
+
 mongoose.connect('mongodb://localhost/applicatondata',{useNewUrlParser:true, useFindAndModify:false, useUnifiedTopology:true})
 .then(()=>{
 	console.log("mongodb connected successfull ")
