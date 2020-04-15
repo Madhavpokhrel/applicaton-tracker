f9a3ba412d2b8d40243687d67bae93cc5efcb160
Madhavpokhrel <Madhavpokhrel05@gmail.com>
2020-04-15 17:04:40 -0400

    fiesl

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
-const express= require('express')
-const PORT  = process.env.PORT||5000
-const applicationRouter= require('./router/applicationRouter')
-const app = express()
+const express= require('express');
+const PORT  = process.env.PORT||5000;
+const applicationRouter= require('./router/applicationRouter');
+const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
 
-app.use(express.json())
-app.use(applicationRouter)
+app.use(express.json());
+app.use(applicationRouter);
 app.use(express.static(path.join(__dirname,'client/build')));
 
-app.get('/',(req,res)=>{
-	res.sendFile(path.join(__dirname, 'client/build','index.html))
-})
-
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/applicatondata',{useNewUrlParser:true, useFindAndModify:false, useUnifiedTopology:true})
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/applicatondata',{useNewUrlParser:true, useFindAndModify:false, useUnifiedTopology:true})
 .then(()=>{
-	console.log("mongodb connected successfull ")
+	console.log("mongodb connected successfull ");
 })
 .catch(err=>{
 	console.log(err)
 })
 
-if(process.env.NODE_ENV==='production'){
+
+app.get('/',(req,res)=>{
+	res.sendFile(path.join(__dirname, 'client/build','index.html));
+});
+
+
+
+if(process.env.NODE_ENV === 'production'){
 	app.use(express.static('client/build'));
 }
 
